aa4bbb8405de5d8ad27c223cb4fb3e6f519f586d
DarDog <samediacc666@gmial.com>
2021-11-04 20:35:38 +1100

    Feat: complete fake auth

--- app.js
+++ app.js
@@ -8,6 +8,14 @@ app.use(bodyParser.json());
 
 mongoose.connect('mongodb://localhost:27017/mestodb', )
 
+app.use((req, res, next) => {
+  req.user = {
+    _id: '6183a7100cf93744add39ce2'
+  };
+
+  next();
+})
+
 app.use('/users', require('./routes/users'))
 
 app.listen(PORT, () => {
