9c32f56904c6ab5e6ba127b05159bc7da46d4838
DarDog <samediacc666@gmial.com>
2021-11-04 17:55:38 +1100

    Init project

---
+++ app.js
@@ -0,0 +1,16 @@
+const http = require('http');
+const server = http.createServer();
+const express = require('express')
+const { PORT = 3000 } = process.env;
+const mongoose = require('mongoose');
+const app = express();
+
+mongoose.connect('mongodb://localhost:27017/mestodb', {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+})
+
+app.listen(PORT, () => {
+  console.log(`App listening on port:${ PORT }`)
+})
