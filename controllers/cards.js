1040c564efb626e398f4b8a026cbdde9e552353b
DarDog <samediacc666@gmial.com>
2021-11-07 16:38:51 +1100

    fix: refactor errors handlers on card controller

--- controllers/cards.js
+++ controllers/cards.js
@@ -29,11 +29,18 @@ module.exports.setCard = (req, res) => {
 
 module.exports.deleteCardById = (req, res) => {
   Cards.findByIdAndDelete(req.params.cardId)
-    .then(card => res.send({ card: card }))
+    .then(card => {
+      if (card === null) {
+        const ERROR_CODE = 404
+        res.status(ERROR_CODE).send({ message: 'Карточка с указанным _id не найдена.' })
+        return
+      }
+      res.send({ card: card })
+    })
     .catch(err => {
       if (err.name === 'CastError') {
-        const ERROR_CODE = 404
-        res.status(ERROR_CODE).send({ message: 'Карточка с указанным _id не найдена' })
+        const ERROR_CODE = 400
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные' })
         return
       }
       const ERROR_CODE = 500
@@ -44,16 +51,18 @@ module.exports.deleteCardById = (req, res) => {
 module.exports.addLikeOnCard = (req, res) => {
   const userId = req.user._id;
   Cards.findByIdAndUpdate(req.params.cardId, { $addToSet: { likes: userId } }, { new: true })
-    .then(card => res.send({ card: card }))
-    .catch(err => {
-      if (err.name === 'ValidationError') {
-        const ERROR_CODE = 400
-        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя.' })
+    .then(card => {
+      if (card === null) {
+        const ERROR_CODE = 404
+        res.status(ERROR_CODE).send({ message: 'Карточка с указанным _id не найдена.' })
         return
       }
+      res.send({ card: card })
+    })
+    .catch(err => {
       if (err.name === 'CastError') {
-        const ERROR_CODE = 404
-        res.status(ERROR_CODE).send({ message: 'Пользователь по указанному _id не найден' })
+        const ERROR_CODE = 400
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные' })
         return
       }
       const ERROR_CODE = 500
@@ -64,16 +73,18 @@ module.exports.addLikeOnCard = (req, res) => {
 module.exports.removeLikeOnCard = (req, res) => {
   const userId = req.user._id;
   Cards.findByIdAndUpdate(req.params.cardId, { $pull: { likes: userId } }, { new: true })
-    .then(card => res.send({ card: card }))
-    .catch(err => {
-      if (err.name === 'ValidationError') {
-        const ERROR_CODE = 400
-        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные при создании пользователя.' })
+    .then(card => {
+      if (card === null) {
+        const ERROR_CODE = 404
+        res.status(ERROR_CODE).send({ message: 'Карточка с указанным _id не найдена.' })
         return
       }
+      res.send({ card: card })
+    })
+    .catch(err => {
       if (err.name === 'CastError') {
-        const ERROR_CODE = 404
-        res.status(ERROR_CODE).send({ message: 'Пользователь по указанному _id не найден' })
+        const ERROR_CODE = 400
+        res.status(ERROR_CODE).send({ message: 'Переданы некорректные данные' })
         return
       }
       const ERROR_CODE = 500
