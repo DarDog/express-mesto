8594b9f49a53bff6ff82c217fedf02249b99b8ba
DarDog <samediacc666@gmial.com>
2021-11-17 10:42:59 +1100

    fix: refactor router cards

--- routes/cards.js
+++ routes/cards.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const router = require('express')
+  .Router();
 const {
   getCards, setCard, deleteCardById, addLikeOnCard, removeLikeOnCard,
 } = require('../controllers/cards');
@@ -7,25 +8,40 @@ const regExp = require('../regExp/regExp');
 
 router.get('/', getCards);
 router.post('/', celebrate({
-  body: Joi.object().keys({
-    name: Joi.string().required().min(2).max(30),
-    link: Joi.string().pattern(regExp).required(),
-  })
+  body: Joi.object()
+    .keys({
+      name: Joi.string()
+        .required()
+        .min(2)
+        .max(30),
+      link: Joi.string()
+        .pattern(regExp)
+        .required(),
+    })
 }), setCard);
 router.delete('/:cardId', celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }).unknown(true)
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .length(24)
+        .hex(),
+    })
 }), deleteCardById);
-router.put('/:cardId/likes',celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }).unknown(true)
+router.put('/:cardId/likes', celebrate({
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .length(24)
+        .hex(),
+    })
 }), addLikeOnCard);
-router.delete('/:cardId/likes',celebrate({
-  params: Joi.object().keys({
-    cardId: Joi.string().alphanum().length(24),
-  }).unknown(true)
+router.delete('/:cardId/likes', celebrate({
+  params: Joi.object()
+    .keys({
+      cardId: Joi.string()
+        .length(24)
+        .hex(),
+    })
 }), removeLikeOnCard);
 
 module.exports = router;
