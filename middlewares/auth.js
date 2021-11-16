395a213a07ce81c4e47dad03d9d17fd194e71118
DarDog <samediacc666@gmial.com>
2021-11-16 17:03:48 +1100

    feat: auth errors update

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -1,12 +1,12 @@
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const ForbiddenError = require('../errors/forbidden-err');
+const AuthError = require('../errors/auth-err')
 
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res
-      .status(403)
-      .send({ message: 'К этому ресурсу есть доступ только для авторизированных пользователей' });
+    next(new ForbiddenError('К этому ресурсу есть доступ только для авторизированных пользователей'))
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,9 +15,7 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, '45ea781744ec7b4e07a1ff7e4adbd95bacff89e3d0266bb0e17a9f12ff31e01e')
   } catch (err) {
-    return res
-      .status(401)
-      .send({ message: 'Необходима авторизация' });
+    next(new AuthError('Необходима авторизация'))
   }
 
   req.user = payload;
