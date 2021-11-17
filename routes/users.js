fbfdc4f960e958524f3159a28bdc3c1fb9a689ee
DarDog <samediacc666@gmial.com>
2021-11-17 11:30:00 +1100

    fix: refactor users route

--- routes/users.js
+++ routes/users.js
@@ -1,4 +1,5 @@
-const router = require('express').Router();
+const router = require('express')
+  .Router();
 const {
   getUsers,
   getUserById,
@@ -6,21 +7,37 @@ const {
   updateUser,
   updateUserAvatar,
 } = require('../controllers/users');
-const { celebrate, Joi } = require('celebrate')
-const regExp = require('../regExp/regExp')
+const { celebrate, Joi } = require('celebrate');
+const regExp = require('../regExp/regExp');
 
 router.get('/', getUsers);
-router.get('/me', getCurrentUserInfo)
+router.get('/me', getCurrentUserInfo);
 router.get('/:userId', celebrate({
-  params: Joi.object().keys({
-    userId: Joi.string().alphanum().length(24),
-  }).unknown(true)
+  params: Joi.object()
+    .keys({
+      userId: Joi.string()
+        .length(24)
+        .hex(),
+    })
 }), getUserById);
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object({
+    name: Joi.string()
+      .min(2)
+      .max(30)
+      .required(),
+    avatar: Joi.string()
+      .pattern(regExp)
+      .required(),
+  })
+}), updateUser);
 router.patch('/me/avatar', celebrate({
-  body: Joi.object().keys({
-    avatar: Joi.string().pattern(regExp)
-})
+  body: Joi.object()
+    .keys({
+      avatar: Joi.string()
+        .pattern(regExp)
+        .required(),
+    })
 }), updateUserAvatar);
 
 module.exports = router;
