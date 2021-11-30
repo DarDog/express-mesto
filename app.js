bc6dbd62719d28c0627dd48629bfc576c9f57efe
DarDog <samediacc666@gmial.com>
2021-11-30 14:24:07 +1100

    feat: add loggers in app

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const { login, setUser } = require('./controllers/users');
 const auth = require('./middlewares/auth');
 const { errorHandler } = require('./middlewares/errors');
 const regExp = require('./regExp/regExp');
+const { requestLogger, errorLogger } = require('./middlewares/Logger');
 
 const { PORT = 3000 } = process.env;
 const app = express();
@@ -15,6 +16,8 @@ app.use(bodyParser.json());
 app.use(cookieParser());
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
+
+app.use(requestLogger);
 
 app.post('/signin', celebrate({
   body: Joi.object()
@@ -52,6 +55,8 @@ app.use(auth);
 app.use('/users', require('./routes/users'));
 app.use('/cards', require('./routes/cards'));
 app.use('/', require('./routes/notFound'));
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(errorHandler);
