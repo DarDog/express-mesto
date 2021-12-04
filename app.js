1bbe08bab44184d888ab64f929977a811b4fdc52
DarDog <samediacc666@gmial.com>
2021-12-04 14:44:59 +1100

    feat: refactor cors

--- app.js
+++ app.js
@@ -13,10 +13,10 @@ const cors = require('./middlewares/cors')
 const { PORT = 3000 } = process.env;
 const app = express();
 
+app.use(cors());
+
 app.use(bodyParser.json());
 app.use(cookieParser());
-
-app.use(cors())
 
 mongoose.connect('mongodb://localhost:27017/mestodb');
 
