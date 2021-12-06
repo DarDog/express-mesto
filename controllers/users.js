0bc08d5b334f45200a03929ea74c95845a825f4f
DarDog <samediacc666@gmial.com>
2021-12-06 18:09:28 +1100

    feat: refactor cookie

--- controllers/users.js
+++ controllers/users.js
@@ -151,7 +151,6 @@ module.exports.login = (req, res, next) => {
               .cookie('token', token, {
                 maxAge: 3600000 * 24 * 7,
                 httpOnly: true,
-                sameSite: 'None',
               })
               .end();
           }
