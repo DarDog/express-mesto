21c1264478ec29a20fe09c6f3eb67bf9e7cf2bd0
DarDog <samediacc666@gmial.com>
2021-12-08 14:20:47 +1100

    feat: final update

--- controllers/users.js
+++ controllers/users.js
@@ -149,7 +149,7 @@ module.exports.login = (req, res, next) => {
               { expiresIn: '7d' },
             );
             res
-              .cookie('token', token, {
+              .cookie('jwt', token, {
                 maxAge: 3600000 * 24 * 7,
                 httpOnly: true,
                 sameSite: true
